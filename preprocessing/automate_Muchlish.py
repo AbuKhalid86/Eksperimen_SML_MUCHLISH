8e29bb2d8dcdda8b65b6df9ba6da26967b9dc095
AbuKhalid86 <surv.massenrempulu>
2025-06-20 23:26:31 +0800

    Initial commit

---
+++ preprocessing/automate_Muchlish.py
@@ -0,0 +1,28 @@
+import pandas as pd
+from sklearn.preprocessing import StandardScaler, LabelEncoder
+
+def preprocess_pipeline(df: pd.DataFrame):
+    # Gunakan kode preprocessing kamu dari Colab, misalnya:
+    df = df.copy()
+    df['PM10'] = df['PM10'].clip(lower=0)
+    df['SO2'] = df['SO2'].clip(lower=0)
+    df['Humidity'] = df['Humidity'].clip(upper=100)
+
+    X = df.drop('Air Quality', axis=1)
+    y = df['Air Quality']
+
+    scaler = StandardScaler()
+    X_scaled = scaler.fit_transform(X)
+
+    le = LabelEncoder()
+    y_encoded = le.fit_transform(y)
+
+    df_cleaned = pd.DataFrame(X_scaled, columns=X.columns)
+    df_cleaned['Air Quality'] = y_encoded
+    return df_cleaned
+
+if __name__ == "__main__":
+    df_raw = pd.read_csv(r'D:\DATA\Digitalent\Proyek MSML\MSML_Submission_Muchlish\dataset\updated_pollution_dataset.csv')
+    df_ready = preprocess_pipeline(df_raw)
+    df_ready.to_csv(r'D:\DATA\Digitalent\Proyek MSML\MSML_Submission_Muchlish\MLProject\dataset_preprocessing.csv', index=False)
+
